7ae74330c01e9fcb819f3341de8892e136a5df7d
LolyaDi <you@example.com>
2019-03-23 12:53:17 +0600

    Двенадцатая домашняя работа

---
+++ Home_Work_12/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Home_Work_12
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public double Potencia { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ Home_Work_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Home_Work_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
